664f02ed08a1a8b9ebff3ddd964c8266943d632f
pk1890 <piotrekkotara@gmail.com>
2020-04-21 13:38:53 +0200

    modified app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello modified World! I have been seen {} times.\n'.format(count)
+    return 'Hello totally  modified World! I have been seen {} times.\n'.format(count)
